76eff9eccb7fe690d23298c9b3cfbcfdb29f9bdb
Christian Schroeder <caschroeder@outlook.com>
2019-01-16 18:08:30 +0000

    bckp

--- exp_scripts_alt/run.sh
+++ exp_scripts_alt/run.sh
@@ -58,7 +58,7 @@ if [ $target == "local" ] ; then
         echo "Starting repeat number $i on GPU $gpu_id"
         HASH=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)
         echo "NV_GPU=${gpu_id} ${ICNN_PATH}/docker.sh ${HASH} python3 train.py ${cmd_line} --exp-name ${name}__repeat${i} &"
-        NV_GPU=${gpu_id} ${ICNN_PATH}/docker.sh ${HASH} python3 train.py ${cmd_line} --exp-name ${name}__repeat${i} &
+        NV_GPU=${gpu_id} ${ICNN_PATH}/docker.sh ${HASH} python3 RL/src/main.py ${cmd_line} --exp-name ${name}__repeat${i} &
         echo "repeat: ${i}"
         echo "    name: ${name}__repeat${i}" >> $sfilepath
         echo "    gpu: ${gpu_id}" >> $sfilepath
