3b95f2c8b1135ac6f0ba1cf7dd1e7fa2a675b100
Christian Schroeer <schroederdewitt@gmail.com>
2019-01-25 13:16:33 +0000

    update naf

---
+++ exp_scripts_alt/baselines/halfcheetah-naf.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -z "$3" ]; then
+    echo "target 'local' selected automatically."
+    target="local"
+    tag=$1
+    reps=$2
+else
+   target=$1
+   tag=$2
+   reps=$3
+fi 
+
+
+seed1=`od -A n -t d -N 2 /dev/urandom`
+seed2=`od -A n -t d -N 2 /dev/urandom`
+seed3=`od -A n -t d -N 2 /dev/urandom`
+cmd_line=" --model NAF --env HalfCheetah-v2 --outdir /icnn/results/${tag} --total 100000 --force --train 100 --test 1 --tfseed ${seed1} --npseed ${seed2} --gymseed ${seed3} "
+
+${ICNN_PATH}/exp_scripts_alt/run.sh "${target}" "${cmd_line}" "${tag}" "${reps}"
